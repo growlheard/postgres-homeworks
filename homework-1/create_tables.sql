8043ee31f00a070cf3eb61788cf64e76cc9fa1be
Growlheard <growlheard@gmail.com>
2023-04-13 12:53:21 +0300

    add scrypt into main.py and sql-code into create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+	first_name varchar(50) NOT NULL,
+	last_name varchar(50) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date date NOT NULL,
+	notes text NOT NULL
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar PRIMARY KEY,
+	company_name varchar(50) NOT NULL,
+	contact_name varchar(50) NOT NULL
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar REFERENCES customers(customer_id) NOT NULL,
+	employee_id int NOT NULL,
+	order_date date  NOT NULL,
+	ship_city varchar(30)  NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,59 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+# Создаем подключение к базе данных Postgres
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='qwerty')
+
+# Читаем данные из CSV-файлов и заполняем списки словарей для каждой таблицы
+employees_data = []
+with open('north_data/employees_data.csv', 'r') as ed:
+    data = csv.DictReader(ed)
+    for i in data:
+        employees_data.append(i)
+
+custumers_data = []
+with open('north_data/customers_data.csv', 'r') as cd:
+    data = csv.DictReader(cd)
+    for i in data:
+        custumers_data.append(i)
+
+orders_data = []
+with open('north_data/orders_data.csv', 'r') as od:
+    data = csv.DictReader(od)
+    for i in data:
+        orders_data.append(i)
+
+try:
+    with conn:
+        with conn.cursor() as cur:
+            # Вставляем данные в таблицу employees
+            for i in range(len(employees_data)):
+                cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s)',
+                            (employees_data[i]['first_name'],
+                             employees_data[i]['last_name'],
+                             employees_data[i]['title'],
+                             employees_data[i]['birth_date'],
+                             employees_data[i]['notes']))
+
+            # Вставляем данные в таблицу customers
+            for i in range(len(custumers_data)):
+                cur.execute('INSERT INTO customers VALUES (%s, %s, %s)',
+                            (custumers_data[i]['customer_id'],
+                             custumers_data[i]['company_name'],
+                             custumers_data[i]['contact_name']))
+
+            # Вставляем данные в таблицу orders
+            for i in range(len(employees_data)):
+                cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)',
+                            (orders_data[i]['order_id'],
+                             orders_data[i]['customer_id'],
+                             orders_data[i]['employee_id'],
+                             orders_data[i]['order_date'],
+                             orders_data[i]['ship_city']))
+
+except psycopg2.Error as e:
+    print(f"Ошибка при вставке данных в базу данных: {e}")
+
+finally:
+    conn.close()
