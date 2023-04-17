1c7bdb690513ca528021cd7ad546fa6ba8943da2
Growlheard <growlheard@gmail.com>
2023-04-17 13:35:45 +0300

    fix Main and create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,26 +1,27 @@
 -- SQL-команды для создания таблиц
-
-CREATE TABLE employees
+create table employees
 (
-	first_name varchar(50) NOT NULL,
-	last_name varchar(50) NOT NULL,
-	title varchar(100) NOT NULL,
-	birth_date date NOT NULL,
-	notes text NOT NULL
+  employee_id int PRIMARY KEY,
+  first_name varchar(100) not null,
+  last_name varchar(100) NOT NULL,
+  title varchar NOT NULL ,
+  birth_date date NOT NULL,
+  notes text NOT NULL
 );
 
 CREATE TABLE customers
 (
-	customer_id varchar PRIMARY KEY,
-	company_name varchar(50) NOT NULL,
-	contact_name varchar(50) NOT NULL
+     customer_id varchar PRIMARY KEY,
+	 company_name varchar NOT NULL,
+	 contact_name varchar NOT NULL
 );
 
 CREATE TABLE orders
 (
-	order_id int PRIMARY KEY,
+    order_id int PRIMARY KEY,
 	customer_id varchar REFERENCES customers(customer_id) NOT NULL,
-	employee_id int NOT NULL,
-	order_date date  NOT NULL,
-	ship_city varchar(30)  NOT NULL
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar NOT NULL
+
 );
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,59 +1,33 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
 import csv
 import psycopg2
 
-# Создаем подключение к базе данных Postgres
 conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='qwerty')
 
-# Читаем данные из CSV-файлов и заполняем списки словарей для каждой таблицы
-employees_data = []
-with open('north_data/employees_data.csv', 'r') as ed:
-    data = csv.DictReader(ed)
-    for i in data:
-        employees_data.append(i)
-
-custumers_data = []
-with open('north_data/customers_data.csv', 'r') as cd:
-    data = csv.DictReader(cd)
-    for i in data:
-        custumers_data.append(i)
-
-orders_data = []
-with open('north_data/orders_data.csv', 'r') as od:
-    data = csv.DictReader(od)
-    for i in data:
-        orders_data.append(i)
 
 try:
     with conn:
         with conn.cursor() as cur:
-            # Вставляем данные в таблицу employees
-            for i in range(len(employees_data)):
-                cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s)',
-                            (employees_data[i]['first_name'],
-                             employees_data[i]['last_name'],
-                             employees_data[i]['title'],
-                             employees_data[i]['birth_date'],
-                             employees_data[i]['notes']))
-
-            # Вставляем данные в таблицу customers
-            for i in range(len(custumers_data)):
-                cur.execute('INSERT INTO customers VALUES (%s, %s, %s)',
-                            (custumers_data[i]['customer_id'],
-                             custumers_data[i]['company_name'],
-                             custumers_data[i]['contact_name']))
-
-            # Вставляем данные в таблицу orders
-            for i in range(len(employees_data)):
-                cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)',
-                            (orders_data[i]['order_id'],
-                             orders_data[i]['customer_id'],
-                             orders_data[i]['employee_id'],
-                             orders_data[i]['order_date'],
-                             orders_data[i]['ship_city']))
-
+            with open("north_data/employees_data.csv") as file:
+                i = 1
+                reader = csv.reader(file)
+                for row in reader:
+                    if '_' not in row[0]:
+                        cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)",
+                                    (i, row[0], row[1], row[2], row[3], row[4]))
+                        i += 1
+            with open("north_data/customers_data.csv") as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    if '_' not in row[0]:
+                        cur.execute("INSERT INTO customers VALUES (%s, %s, %s)",
+                                    (row[0], row[1], row[2]))
+            with open("north_data/orders_data.csv") as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    if '_' not in row[0]:
+                        cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)",
+                                    (row[0], row[1], row[2], row[3], row[4]))
 except psycopg2.Error as e:
     print(f"Ошибка при вставке данных в базу данных: {e}")
-
 finally:
     conn.close()
